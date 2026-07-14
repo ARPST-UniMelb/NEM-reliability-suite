12ad39e3ced87cfbf07064358bf04b203c0774e5
Tim Kopka <tim.kopka@student.unimelb.edu.au>
2026-07-14 14:47:34 +1000

    Clean up functions and updated Manifest for reproducability

--- functions/assess_adequacy_resilience_VRE_drought.jl
+++ functions/assess_adequacy_resilience_VRE_drought.jl
@@ -34,7 +34,6 @@ function assess_adequacy_resilience_VRE_drought(target_year::Int=2030,
         default_horizon::Int=4, min_time_after_event::Int=4, 
         optimisation_window::Int=48, move_forward::Int=24,
         resilience_event::String="",
-        saving_details=(:shortfall, :stor_energy, :drs_borrowing, :genstor_energy),
         regions_vre_drought::Vector{Int} = collect(1:12),
         days_vre_drought::Int=1)
 
@@ -71,7 +70,6 @@ function assess_adequacy_resilience_VRE_drought(target_year::Int=2030,
     base_folder_pras = joinpath(base_path, "pras-files", case_name)
     base_folder_schedules = joinpath(base_path, "schedules", case_name)
     base_folder_results = joinpath(base_path, "results", case_name)
-    base_folder_reoptimisation_results = joinpath(base_path, "reoptimisation-results", case_name)
 
     pisp_input_folder = joinpath(base_folder_pisp, "out-ref$(reference_trace)-poe$(poe)", "csv")
     timeseries_folder = "schedule-$(target_year)"
@@ -79,11 +77,9 @@ function assess_adequacy_resilience_VRE_drought(target_year::Int=2030,
     if resilience_event != ""
         schedules_folder = joinpath(base_folder_schedules, "out-ref$(reference_trace)-poe$(poe)-$(resilience_event)", "ty$(target_year)")
         results_folder = joinpath(base_folder_results, "out-ref$(reference_trace)-poe$(poe)-$(resilience_event)", "ty$(target_year)")
-        reoptimisation_results_folder = joinpath(base_folder_reoptimisation_results, "out-ref$(reference_trace)-poe$(poe)-$(resilience_event)", "ty$(target_year)")
     else
         schedules_folder = joinpath(base_folder_schedules, "out-ref$(reference_trace)-poe$(poe)", "ty$(target_year)")
         results_folder = joinpath(base_folder_results, "out-ref$(reference_trace)-poe$(poe)", "ty$(target_year)")
-        reoptimisation_results_folder = joinpath(base_folder_reoptimisation_results, "out-ref$(reference_trace)-poe$(poe)", "ty$(target_year)")
     end
 
     println(Threads.nthreads(), " threads available for parallel processing.")
@@ -218,19 +214,11 @@ function assess_adequacy_resilience_VRE_drought(target_year::Int=2030,
                 hydro_parameters=hydro_parameters,
                 optimisation_window=optimisation_window, move_forward=move_forward, 
                 optimiser_name=solver, input_folder=pisp_input_folder,
-                genOpDetails=genOpDetails,
-                saving_details=saving_details);
+                genOpDetails=genOpDetails);
 
-        if typeof(sf_new) == Array{Int, 3}
-            # This means that reoptimisation output already includes the original shortfall, so we don't need to add it again (this is a legacy option!)
-            SchedNEM.saveSfMatrix(sf_new, filename_output)
-            eens_reoptimised[i] = sum(sf_new) ./ sample_number_per_run
-        else
-            SchedNEM.saveSfMatrix(sf_new.shortfall .+ res.shortfall, filename_output)
-            SchedNEM.save_schedule_change(sf_new, joinpath(reoptimisation_results_folder, "schedule_changes_s$(scenario)_batch$(i).csv"))
-            eens_reoptimised[i] = sum(sf_new.shortfall .+ res.shortfall) ./ sample_number_per_run
-        end
-        
+        SchedNEM.saveSfMatrix(sf_new, filename_output)
+        eens_reoptimised[i] = sum(sf_new) ./ sample_number_per_run
+
     end
     @info "Finished system response analysis."
 
